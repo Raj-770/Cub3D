e19a10f39512f1c05f34782d47f7d866b55f27dc
Fayeza99 <fayezanikzad@yahoo.com>
2024-05-15 18:45:44 +0200

    worked with players angle

--- src/draw_map.c
+++ src/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: fnikzad <fnikzad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 14:53:56 by fnikzad           #+#    #+#             */
-/*   Updated: 2024/05/15 14:54:46 by fnikzad          ###   ########.fr       */
+/*   Updated: 2024/05/15 18:44:20 by fnikzad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,47 @@ int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 
 void	draw_square(void *mlx_img, int start_x, int start_y, int size, int color)
 {
-    int x;
-    int y;
-
-    y = start_y;
-    while (y < start_y + size)
-    {
-        x = start_x;
-        while (x < start_x + size)
-        {
-            mlx_put_pixel(mlx_img, x, y, color);
-            x++;
-        }
-        y++;
-    }
+	int x;
+	int y;
+	
+	y = start_y;
+	while (y < start_y + size)
+	{
+		x = start_x;
+		while (x < start_x + size)
+		{
+			mlx_put_pixel(mlx_img, x, y, color);
+			x++;
+		}
+		y++;
+	}
 }
+
+void mlx_draw_line(void *mlx_img, int x0, int y0, int x1, int y1, int color)
+{
+	int dx = abs(x1 - x0);
+	int dy = abs(y1 - y0);
+	int sx = x0 < x1 ? 1 : -1;
+	int sy = y0 < y1 ? 1 : -1;
+	int err = dx - dy;
+
+	while (x0 != x1 || y0 != y1)
+	{
+		mlx_put_pixel(mlx_img, x0, y0, color);
+		int err2 = 2 * err;
+		if (err2 > -dy)
+		{
+			err -= dy;
+			x0 += sx;
+		}
+		if (err2 < dx)
+		{
+			err += dx;
+			y0 += sy;
+		}
+	}
+}
+
 
 
 void	drawMap2D(void *param)
@@ -65,4 +91,10 @@ void	drawMap2D(void *param)
 	}
 	player_color = ft_pixel(0, 255, 0, 255);
 	draw_square(game->mlx_img, game->player->px, game->player->py, 8, player_color);
+
+	int line_length = 20; // Length of the line segment
+	int line_color = ft_pixel(0, 255, 0, 255); // Blue color for the line
+	int line_end_x = game->player->px + line_length * cos(game->player->p_a);
+	int line_end_y = game->player->py + line_length * sin(game->player->p_a);
+	mlx_draw_line(game->mlx_img, game->player->px, game->player->py, line_end_x, line_end_y, line_color);
 }
--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpambhar <rpambhar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fnikzad <fnikzad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 16:28:03 by rpambhar          #+#    #+#             */
-/*   Updated: 2024/05/15 15:14:23 by rpambhar         ###   ########.fr       */
+/*   Updated: 2024/05/15 15:21:09 by fnikzad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ int	main(int argc, char **argv)
 	t_cub		game;
 
 	init_map_data(&map_data);
-	init(&game);
 	if (!parser(argc, argv, &map_data))
 		return (0);
 	game.data = &map_data;
+	init(&game);
 	display(&game);
 	mlx_loop_hook(game.mlx_ptr, drawMap2D, &game);
 	mlx_loop_hook(game.mlx_ptr, ft_hook, &game);
