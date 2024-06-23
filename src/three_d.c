f9823f602180d0d4b417ca07906b794d79c57ff5
Rajkumar Pambhar <rajkumarpambhar770@gmail.com>
2024-06-23 18:32:15 +0200

    Fixed the fish eye effect

--- src/three_d.c
+++ src/three_d.c
@@ -6,7 +6,7 @@
 /*   By: rpambhar <rpambhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 17:42:02 by fnikzad           #+#    #+#             */
-/*   Updated: 2024/06/23 16:22:43 by rpambhar         ###   ########.fr       */
+/*   Updated: 2024/06/23 18:31:35 by rpambhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ int	draw_line_color(mlx_image_t *image, t_v_line line, int color)
 
 int	draw_line_3d(t_cub *game, t_v_line line, mlx_texture_t *tex)
 {
+	(void)tex;
 	double deltax = line.end_x - line.begin_x;
 	double deltay = line.end_y - line.begin_y;
 	int pixels = (int)sqrt((deltax * deltax) + (deltay * deltay));
@@ -60,32 +61,14 @@ int	draw_line_3d(t_cub *game, t_v_line line, mlx_texture_t *tex)
 	deltax /= pixels;
 	deltay /= pixels;
 
-	float texture_step = (float)tex->height / (line.end_y - line.begin_y);
-	float texture_pos = 0.0f;
-
-	while (pixels > 0) {
-		if (pixelx >= 0 && pixelx < WIDTH && pixely >= 0 && pixely < HEIGHT) {
-			int tex_x = (int)((pixelx - line.begin_x) * tex->width / \
-			(line.end_x - line.begin_x));
-			int tex_y = (int)texture_pos;
-			tex_x = tex_x % tex->width;
-			tex_y = tex_y % tex->height;
-			if (tex_x >= 0 && tex_x < (int)tex->width && tex_y >= 0 && tex_y < \
-			(int)tex->height) {
-				uint8_t *pixel = tex->pixels + (tex_y * tex->width + tex_x) * \
-				tex->bytes_per_pixel;
-				uint32_t red = pixel[0];
-				uint32_t green = pixel[1];
-				uint32_t blue = pixel[2];
-				uint32_t alpha = pixel[3];
-				int color = (alpha << 24) | (red << 16) | (green << 8) | blue;
-				(void)color;
-				mlx_put_pixel(game->mlx_img, (int)pixelx, (int)pixely, ft_pixel(255, 0, 0, 255));
-			}
+	while (pixels > 0)
+	{
+		if (pixelx >= 0 && pixelx < WIDTH && pixely >= 0 && pixely < HEIGHT)
+		{
+			mlx_put_pixel(game->mlx_img, (int)pixelx, (int)pixely, ft_pixel(255, 0, 0, 255));
 		}
 		pixelx += deltax;
 		pixely += deltay;
-		texture_pos += texture_step;
 		--pixels;
 	}
 	return true;
@@ -96,9 +79,15 @@ void	three_d(t_cub *game, int i)
 	t_v_line	line;
 	float		height;
 	float		lineoff;
+	float		ca;
 
+	ca = game->player->p_a - game->ray.ra;
+	if (ca < 0)
+		ca += 2 * PI;
+	if (ca > 2 * PI)
+		ca -= 2 * PI;
+	game->ray.dist = game->ray.dist * cos(ca);
 	height = (HEIGHT * game->block_size) / game->ray.dist;
-	game->ray.dist = game->ray.dist * cos(game->ray.ra - game->player->p_a);
 	if (height > HEIGHT)
 		height = HEIGHT;
 	lineoff = HEIGHT / 2 - height / 2;
